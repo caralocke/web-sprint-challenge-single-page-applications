66c398d36953a3ae9ecf9e1dbf6857bbd127649e
caralocke <cara.m.locke@gmail.com>
2021-05-21 07:35:22 -0400

    Created a few components I'll need and working on App.js currently.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
 import React from "react";
+import {Route} from 'react-router-dom'
+import Home from './components/Home'
+import Pizza from './components/Pizza'
+
+const initialFormValues = {
+  name: '',
+  size: '',
+  topping1: false,
+  topping2: false,
+  topping3: false,
+  topping4: false,
+  special: '',
+}
 
 const App = () => {
   return (
