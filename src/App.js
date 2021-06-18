d9ef5d8b40232b2427e332210625e4306db4e77a
caralocke <cara.m.locke@gmail.com>
2021-06-18 06:18:02 -0400

    Created a form with an id of pizza-form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useState } from "react"; //Imported useState
 import { Link, Route, Switch } from 'react-router-dom'; //Import Route and Switch
 import Home from './components/Home' //Imported the Home Component
 import OrderForm from './components/OrderForm' //Imported OrderForm
 
+//create initial form values
+const initialFormValues = {
+  name: ''
+}
+
 const App = () => {
+
+  const [ formValues, setFormValues ] = useState(initialFormValues)
+
+  //write a function to handle input changes
+  const inputChange = (name, value) => {
+    setFormValues({...formValues, [name] : value})
+  }
   return (
     <>
       <h1>Lambda Eats</h1>
       <Link to='/pizza' id='order-pizza'>Order Now!</Link> {/*Add Link to OrderForm with the id 'order-pizza'*/}
 
       <Switch>
+
         <Route path='/pizza'> {/*Add Route with path of '/pizza' for the OrderForm component */}
-          <OrderForm />
+          <OrderForm values={formValues} change={inputChange}/> {/*Passing props to OrderForm */}
         </Route>
 
         <Route path='/'> {/*Add Route with path of '/' for the Home component*/}
           <Home />
         </Route>
+
       </Switch>
     </>
   );
--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -1,10 +1,18 @@
 //Created a component for my order form
 import React from 'react'
 
-export default function OrderForm() {
+export default function OrderForm(props) {
+    const { values, change } = props
+
+    //create a function to handle the onChange event for the inputs
+    const onChange = evt => {
+        const { name, value } = evt.target
+        change(name, value)
+    }
+
     return (
-        <div>
+        <form id='pizza-form'>
             
-        </div>
+        </form>
     )
 }
