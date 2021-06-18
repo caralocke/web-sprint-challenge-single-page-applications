ee7a494de157303dc570c7ec776db3b60a2e2548
caralocke <cara.m.locke@gmail.com>
2021-06-18 07:46:38 -0400

    Wrote a cypress test to make sure that multiple toppings can be selected.

--- cypress/integration/sprint.spec.js
+++ cypress/integration/sprint.spec.js
@@ -6,9 +6,20 @@ describe('Lambda Eats App', () => {
 
     const nameInput = () => cy.get('#name-input')
     const specialInput = () =>  cy.get('#special-text')
+    const cheeseInput = () => cy.get('input[name=cheese]')
+    const pepperoniInput = () => cy.get('input[name=pepperoni]')
+    const sausageInput = () => cy.get('input[name=sausage]')
+    const vegetableInput = () => cy.get('input[name=vegetable]')
 
     it('Can add text to the box', () =>{
         nameInput().type('Name')
         specialInput().type('Handle with care')
+    })
+
+    it('Can select multiple toppings', () => {
+        cheeseInput().click()
+        pepperoniInput().click()
+        sausageInput().click()
+        vegetableInput().click()
     })
 })
